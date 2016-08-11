fd065c2ff41d4777500870f8590cb8e9f5f9626c
Markus Reiter <me@reitermark.us>
2016-08-11 15:45:01 +0200

    updated yate (3.13.0.2)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
-  version '3.12'
-  sha256 '6611b12ef7336e1add86e35345aa2605fde79248c23354736a14384482a72eff'
+  version '3.13.0.2'
+  sha256 '9da3fc54c448f6ea970addaef98027e190ce48c51b89ce8de48f8409c80b6d9b'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
       user_agent: :fake
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '50770d2e20a73b0d2cdbd4ad663650e3ee1d963431dfb23b803912df950573b8'
+          checkpoint: '454bc6ab0a129b84b251793bae9fff91de0ad0d59ab9c26027dfd096fac2b87f'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
